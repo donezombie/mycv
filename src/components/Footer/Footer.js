8fafa2f0e3a0da89b7b3f8b660f13bc56b8fcc07
Phạm Phú Quý Đôn <donezombie@gmail.com>
2019-02-05 03:04:35 +0700

    .

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
     return (
       <Col md={{size: 8, offset: 2}}>
         <div className="h-100 d-flex justify-content-center align-items-center">
-          <p className="text-light">Sample Text</p>
+          <p className="text-light">Here is Fốter</p>
         </div>
       </Col>
     )
--- src/components/Wrap/AboutMe.js
+++ src/components/Wrap/AboutMe.js
@@ -14,7 +14,7 @@ class AboutMe extends Component {
                     <div id="aboutText" className="col-md-5">
                         <h3 className="pb-3 font-weight-bold" style={{display: "none"}}>About Me</h3>
                         <p>Hi guys! I'm a Front-end Web Developer.</p>
-                        <p id="aboutTextP">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+                        <p id="aboutTextP">I don't know what to write here... @@</p>
                     </div>
                     <div className="col-md-7 d-flex justify-content-center">
                       <h3 className="pb-3 font-weight-bold" style={{display: "none"}}>Profile</h3>
